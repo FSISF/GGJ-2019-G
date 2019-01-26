d2e61940cecffb5d689b24c2d9493c0f25d36a39
timtim <tim12332000@gmail.com>
2019-01-26 15:19:47 +0800

    Add AI Test

---
+++ Assets/Script/CharManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharManager : MonoBehaviour
+{
+	public List<CharInterface> _charList;
+
+	public static CharManager Instance;
+
+	public void Register(CharInterface charInterface)
+	{
+		_charList.Add(charInterface);
+	}
+
+	public void UnRegister(CharInterface charInterface)
+	{
+		_charList.Remove(charInterface);
+	}
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+}

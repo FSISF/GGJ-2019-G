a24546985d111935de630342aec7c6560f9e860f
FSISF <a1455785@gmail.com>
2019-01-26 18:17:19 +0800

    更新Tilemap 新增障礙物

---
+++ Assets/TileMap/room.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="room" tilewidth="48" tileheight="48" tilecount="9" columns="3">
+ <image source="room.png" width="144" height="144"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="0" width="48" height="23.75"/>
+   <object id="2" x="0.125" y="0" width="24.625" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="0.125" width="48" height="23.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="0" width="48" height="23.875"/>
+   <object id="2" x="23.25" y="0" width="24.625" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="0" width="25" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="23.125" y="-0.125" width="24.875" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="24.25" height="48.125"/>
+   <object id="3" x="0.5" y="24.25" width="47.625" height="23.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="24.125" width="47.875" height="23.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="23.125" y="-0.125" width="25" height="48"/>
+   <object id="2" x="0.125" y="24.125" width="47.875" height="23.75"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Assets/TileMap/room_2.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="room_2" tilewidth="48" tileheight="48" tilecount="9" columns="3">
+ <image source="room_2.png" width="144" height="144"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="29.125" y="29.125" width="18.875" height="18.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="29" width="48" height="19.125"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="29" width="18.625" height="19"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="29.125" y="0" width="18.875" height="47.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="0" width="19" height="48"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="29" y="0" width="19.125" height="18.875"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.125" width="48" height="18.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="0.125" width="18.75" height="18.875"/>
+  </objectgroup>
+ </tile>
+</tileset>

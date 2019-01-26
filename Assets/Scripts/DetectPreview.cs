f8c8bda7b2470d573148d0b2a20c1420721885ef
timtim <tim12332000@gmail.com>
2019-01-26 21:38:11 +0800

    detect Preview fix , add shoot bullet Node

--- Assets/Scripts/DetectPreview.cs
+++ Assets/Scripts/DetectPreview.cs
@@ -5,34 +5,30 @@ namespace Battle
 	public class DetectPreview : MonoBehaviour
 	{
 		public Color DetectColor = new Color(1, 0, 0, 0.1f);
-		public Color ShootColor = new Color(0, 1, 0, 0.1f);
+		//public Color ShootColor = new Color(0, 1, 0, 0.1f);
 
-		public float DetectAngle = 360;
+		//public float DetectAngle = 360;
 		public float DetectRange = 5f;
-		public float ShootRangeMax = 5f;
+		//public float ShootRangeMax = 5f;
 
 		public bool IsSelectedShow;
 
 		private void Show()
 		{
-			float theta = (DetectAngle + 180) / 360f / 2;
+			//float theta = (DetectAngle + 180) / 360f / 2;
+			
 
-
-			Vector3 v = new Vector3(Mathf.Cos(theta * 2 * Mathf.PI), 0f, Mathf.Sin(theta * 2 * Mathf.PI)).normalized;
+			//Vector3 v = new Vector3(Mathf.Cos(theta * 2 * Mathf.PI), 0f, Mathf.Sin(theta * 2 * Mathf.PI)).normalized;
 #if UNITY_EDITOR
 			UnityEditor.Handles.color = DetectColor;
-			UnityEditor.Handles.DrawSolidArc(transform.position, transform.up, v, DetectAngle, DetectRange);
-
-			UnityEditor.Handles.color = ShootColor;
-			UnityEditor.Handles.DrawSolidArc(transform.position, transform.up, v, DetectAngle, ShootRangeMax);
-
+			UnityEditor.Handles.DrawSolidDisc(transform.position, new Vector3(0, 0, 1), DetectRange);// v, DetectAngle, DetectRange);
 #endif
 
 		}
 
 		private void OnDrawGizmos()
 		{
-			if (IsSelectedShow ==false)
+			if (IsSelectedShow == false)
 			{
 				Show();
 			}
@@ -44,7 +40,7 @@ namespace Battle
 			{
 				Show();
 			}
-			
+
 		}
 	}
 }

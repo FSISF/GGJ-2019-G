fce14cb7073671a01783c614891b9ae18a2745ff
Louis <lou12331@gmail.com>
2019-01-26 12:47:13 +0800

    CharacterState PlayerAttack

--- Assets/Louis/xScript/PlayerController.cs
+++ Assets/Louis/xScript/PlayerController.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using PlayMaker;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 2;
     public Rigidbody2D rb;
     public Vector2 MoveVelocity;
+    public CharacterState state;
+    public Transform FollowPoint;
+    public float AttackCD = 0.5f;
+    public bool CanBite;
+    bool biting;
+    public PlayMakerFSM AttackFSM;
+    Tweener BiteTween;
+    Collision2D lastCollision;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +27,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Speed = state.WalkSpeed;
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         MoveVelocity = moveInput.normalized * Speed;
     }
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + MoveVelocity * Time.fixedDeltaTime);
+        //
+        if (Input.GetMouseButtonDown(0) && CanBite)
+        {
+            AttackFSM.SendEvent("Bite");
+            BiteTween = rb.DOMove(FollowPoint.position, 0.2f).OnPlay(BiteStart).OnComplete(BiteEnd);
+        }
+        else
+        {
+
+        }
+        if (!biting)
+            rb.MovePosition(rb.position + MoveVelocity * Time.fixedDeltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //BiteTween.Kill();
+        //lastCollision = collision;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+    }
+    void BiteStart()
+    {
+        biting = true;
+    }
+    void BiteEnd()
+    {
+        biting = false;
     }
 }

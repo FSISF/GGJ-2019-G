21ffed595300df14d3cc59dd24cfdac1c14bf705
timtim <tim12332000@gmail.com>
2019-01-27 12:57:44 +0800

    fix ui

---
+++ Assets/Script/MonsterAtkNodeMono.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JFun.Gameplay.BehaviourTree;
+using UnityEngine;
+
+public class MonsterAtkNodeMono : AINodeMono
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public class MonsterAtkNode : AINode
+{
+	public override AINodeResult Execute(ref AINode lastRunningNode, AIMember aiMember)
+	{
+
+
+		return AINodeResult.Success;
+	}
+}

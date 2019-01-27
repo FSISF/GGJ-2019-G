278d645c8707ca9c2fdc2defe3884796f9a26858
FSISF <a1455785@gmail.com>
2019-01-27 15:09:16 +0800

    敵人死亡

--- Assets/Script/CharInterface.cs
+++ Assets/Script/CharInterface.cs
@@ -5,82 +5,89 @@ using UnityEngine;
 
 public enum Team
 {
-	Monster,
-	Hero,
+    Monster,
+    Hero,
 }
 
 public class CharInterface : MonoBehaviour
 {
-	public int HpMax = 10;
-	public int Hp = 10;
-	public event Action OnHpZero = delegate { };
+    public int HpMax = 10;
+    public int Hp = 10;
+    public event Action OnHpZero = delegate { };
     public event Action OnHPChange = delegate { };
-	public bool IsDead = false;
+    public bool IsDead = false;
 
-	[SerializeField]
-	private MonsterNav _monsterNav;
-	public Team Team;
-	CharacterState charState;
+    [SerializeField]
+    private MonsterNav _monsterNav;
+    public Team Team;
+    CharacterState charState;
 
-	public void TakeDamage(int damage)
-	{
-		if (IsDead)
-			return;
+    public void TakeDamage(int damage)
+    {
+        if (IsDead)
+            return;
 
 
-		if (Team == Team.Monster)
-		{
+        if (Team == Team.Monster)
+        {
+            Debug.Log(GetComponent<Animator>());
+            GetComponent<Animator>()?.SetTrigger("Hit");
+            GetComponent<CharacterState>()?.Hit();
+        }
 
-			GetComponent<Animator>()?.SetTrigger("Hit");
-			GetComponent<CharacterState>()?.Hit();
-		}
-
-		Hp -= damage;
+        Hp -= damage;
         OnHPChange.Invoke();
 
         if (Hp <= 0)
-		{
-			Hp = 0;
-			IsDead = true;
-			GetComponent<CharacterState>().OnDeathTrigger();
-			OnHpZero.Invoke();
+        {
+            Hp = 0;
+            IsDead = true;
+            GetComponent<CharacterState>().OnDeathTrigger();
+            OnHpZero.Invoke();
 
-			if (CharManager.Instance == null)
-			{
-				Debug.LogFormat("[CharInterface.Start] Not Found CharManager.Instance");
-				return;
-			}
+            if (Team == Team.Monster)
+            {
+                _monsterNav.SwitchNav(NavType.Stop);
+                GetComponent<Animator>()?.Play("Dead");
+         
+            }
 
-			CharManager.Instance.UnRegister(this);
-			//TODO Death Show
-		}
-	}
+            if (CharManager.Instance == null)
+            {
+                Debug.LogFormat("[CharInterface.Start] Not Found CharManager.Instance");
+                return;
+            }
 
-	public void ChangeNav(NavType nav)
-	{
-		if (_monsterNav != null)
-		{
-			_monsterNav.SwitchNav(nav);
-		}
-		else
-		{
-			Debug.LogError("Not Found ");
-		}
-	}
+            CharManager.Instance.UnRegister(this);
+            //TODO Death Show
+        }
+    }
 
-	private void Start()
-	{
-		Hp = HpMax;
-		if (CharManager.Instance == null)
-		{
-			Debug.LogFormat("[CharInterface.Start] Not Found CharManager.Instance");
-			return;
-		}
-		CharManager.Instance.Register(this);
-	}
+    public void ChangeNav(NavType nav)
+    {
+        if (_monsterNav != null)
+        {
+            _monsterNav.SwitchNav(nav);
+        }
+        else
+        {
+            Debug.LogError("Not Found ");
+        }
+    }
 
-	private void Awake()
-	{
-		charState = GetComponent<CharacterState>();
-	}
+    private void Start()
+    {
+        Hp = HpMax;
+        if (CharManager.Instance == null)
+        {
+            Debug.LogFormat("[CharInterface.Start] Not Found CharManager.Instance");
+            return;
+        }
+        CharManager.Instance.Register(this);
+    }
+
+    private void Awake()
+    {
+        charState = GetComponent<CharacterState>();
+    }
 }

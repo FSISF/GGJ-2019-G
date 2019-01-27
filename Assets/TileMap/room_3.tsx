e393668b9f1bc692d125934402159ed5539b1f83
FSISF <a1455785@gmail.com>
2019-01-27 12:45:30 +0800

    全部房間佈置完成

---
+++ Assets/TileMap/room_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="room_3" tilewidth="48" tileheight="48" tilecount="9" columns="3">
+ <image source="room.png" width="144" height="144"/>
+</tileset>

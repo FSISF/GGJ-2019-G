46a8c7aed73996e88e6522c8146de5f6176e72a7
timtim <tim12332000@gmail.com>
2019-01-27 13:44:16 +0800

    stage 1 add nav

--- Assets/Script/MonsterAtkNodeMono.cs
+++ Assets/Script/MonsterAtkNodeMono.cs
@@ -1,28 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using JFun.Gameplay.BehaviourTree;
 using UnityEngine;
 
 public class MonsterAtkNodeMono : AINodeMono
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float MoveDuration;
+	public int Damage = 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public MonsterAtkNode Create()
+	{
+		return new MonsterAtkNode(MoveDuration, Damage);
+	}
 }
 
 public class MonsterAtkNode : AINode
 {
+	private float _duration;
+	private int _damage;
+
+	public MonsterAtkNode(float duration, int damage)
+	{
+		_duration = duration;
+		_damage = damage;
+	}
+
 	public override AINodeResult Execute(ref AINode lastRunningNode, AIMember aiMember)
 	{
-
+		GGAIMember member = aiMember as GGAIMember;
+		CharInterface player = CharManager.Instance.MainChar;
+		member.Self.transform.DOMove(player.transform.position, 0.3f).OnComplete(()=>
+		{
+			player.TakeDamage(_damage);
+		});
 
 		return AINodeResult.Success;
 	}
